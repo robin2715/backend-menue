4c679d12ace445a4d1174c8d3bb9ac9f91b93f05
cristiansanchez2715 <cristiansanchez2715@gmail.com>
2024-12-16 16:53:48 -0500

    actualizacion envio mesero

--- index.js
+++ index.js
@@ -127,10 +127,10 @@ io.emit("cliente_desconectado", tableNumber)
     }) 
   
     socket.on('enviar_mesero', (tableNumber) => {
-      io.to(tableNumber).emit('activar_boton_cliente');
+      // io.to(tableNumber).emit('activar_boton_cliente');
       // io.emit('desactivar_boton_admin', tableNumber);
 
-  io.to(tableNumber).emit('mesero_enviado', tableNumber); // Confirmar que el mesero fue enviado
+  io.emit('mesero_enviado', tableNumber); // Confirmar que el mesero fue enviado
 console.log("SE HA ENVIADO UN MESERO A LA MESA " + tableNumber + " EL BOTON EN EL ADMIN DEBE DESACIVARSE Y EN EL CLIENTE ACTIVARSE")    
 });
   });;
